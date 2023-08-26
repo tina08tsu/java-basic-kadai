2418e4eaee1dd3fbc27f62c58cc47d3d5afd6900
tina08tsu <142229920+tina08tsu@users.noreply.github.com>
2023-08-26 11:45:41 +0900

    Add files via upload

---
+++ Score_Chapter04.java
@@ -0,0 +1,34 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		int d = 40;
+		int e = 50;
+		int f = 60;
+		int g = 70;
+		int h = 80;
+		int i = 90;
+		int j = 100;
+		
+		System.out.println("Aさんの点数は" + a +"点");
+		System.out.println("Bさんの点数は" + b +"点");
+		System.out.println("Cさんの点数は" + c +"点");
+		System.out.println("Dさんの点数は" + d +"点");
+		System.out.println("Eさんの点数は" + e +"点");
+		System.out.println("Fさんの点数は" + f +"点");
+		System.out.println("Gさんの点数は" + g +"点");
+		System.out.println("Hさんの点数は" + h +"点");
+		System.out.println("Iさんの点数は" + i +"点");
+		System.out.println("Jさんの点数は" + j +"点");
+		
+		int average = (a + b + c + d + e + f + g + h + i + j)/10;
+		System.out.println("テストの平均点は" + average + "点");
+
+	} 
+
+}
